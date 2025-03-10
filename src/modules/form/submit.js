a1aba66e3f7f504314e4cd509d8e63728fa17ad1
Rafael Roberto de Oliveira <rafael.roberto200618@gmail.com>
2025-03-10 20:52:43 -0300

    send a selected time to API

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -1,6 +1,7 @@
 import dayjs from "dayjs";
 
 const form = document.querySelector("form");
+const clientName = document.querySelector("#client");
 const selectedDate = document.querySelector("#date");
 
 // Data atual para formatar o input.
@@ -13,4 +14,28 @@ selectedDate.min = inputToday;
 form.onsubmit = (event) => {
   // Previne o comportamento padrão de carregar a página.
   event.preventDefault();
+
+  try {
+    // Recuperando nome do cliente
+    const name = clientName.value.trim(); // remove espaços desnecessários.
+    if(!name) return alert("Informe o nome do cliente!");
+
+    // Recupera o horário selecionado.
+    const hourSelected = document.querySelector(".hour-selected");
+    if(!hourSelected) return alert("Selecione a hora!");
+
+    // Recuperar somente a hora.
+    const [hour] = hourSelected.innerText.split(":");
+    console.log(hour)
+
+    // Insere a hora na data.
+    const when = dayjs(selectedDate.value).add(hour, "hour");
+
+    // Gera um ID.
+    const id = new Date().getTime();
+
+  } catch (error) {
+    alert("Não foi possível realizar o agendamento!");
+    console.log(error);
+  }
 }
